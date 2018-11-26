aa204f57511b830c5a2dd9eabd7dd18f30add19d
Nitin Mahadik <nmahadi1@binghamton.edu>
2018-11-25 23:29:39 -0500

    jdbc update

--- JavaPractice/JdbcConnection.java
+++ JavaPractice/JdbcConnection.java
@@ -3,24 +3,43 @@ package JavaPractice;
 import java.sql.*;
 
 /**
- * First Step: Add the JAR file ojdbcXX.jar to the build path
+ * First Step: Add the JAR file ojdbc8.jar to the build path
  * Ctrl+Alt+Shift+S --> Modules --> Dependencies --> + button on right side.
  */
 public class JdbcConnection {
     public static void main(String[] args) {
+
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet res = null;
+
+        //OTHER WAY : THis way con, stmt, rs becomes part of try and java automatically closes these after use.
+        /*try(Connection con = getConnection(url, username, password, "org.postgresql.Driver");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+        ) {
+            //EXECUTE QUERIES.....
+        }*/
+
         try{
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "nitin", "system");
-            Statement stmt = con.createStatement();
-            ResultSet res = stmt.executeQuery("SELECT * FROM EMP");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "nitin", "system");
+            stmt = con.createStatement();
+            res = stmt.executeQuery("SELECT * FROM EMP");
             while(res.next()){
                 System.out.println(res.getInt(1)+"  "+res.getString(2)+"  "+res.getString(3));
             }
+
+            con.close();
 
         } catch (ClassNotFoundException e){
             e.printStackTrace();
         }catch (SQLException e){
             e.printStackTrace();
+        }finally { // Or we can create a helper class to close the below connections.
+            try { res.close(); } catch (Exception e) { /* ignored */ }
+            try { stmt.close(); } catch (Exception e) { /* ignored */ }
+            try { con.close(); } catch (Exception e) { /* ignored */ }
         }
     }
 }
